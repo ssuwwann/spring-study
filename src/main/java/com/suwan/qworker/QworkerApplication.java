0e38478bf85a0c1f7d1facefb438511db4c07e79
suwan <leesuwan89@gamil.com>
2025-05-23 16:29:21 +0900

    초기화 커밋

---
+++ src/main/java/com/suwan/qworker/QworkerApplication.java
@@ -0,0 +1,13 @@
+package com.suwan.qworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QworkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QworkerApplication.class, args);
+  }
+
+}
